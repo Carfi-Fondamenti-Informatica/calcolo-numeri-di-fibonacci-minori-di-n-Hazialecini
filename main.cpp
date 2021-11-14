c483a4120dc5036b3e7842466bed1ed69e90a9dc
Hazialecini <91554788+Hazialecini@users.noreply.github.com>
2021-11-14 18:20:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   int a=1, b=1, c=1, l=0;
+   cin>>l;
+   if(l>=c){
+      cout<<c<<endl;
+      for(;c<=l;){
+         cout<<c<<endl;
+         c=b+a;
+         b=a;
+         a=c;
+      }
+   }
    return 0;
 }
